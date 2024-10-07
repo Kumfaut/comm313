13615d0049a3bfb0fdb54d1bd621000d730f873f
Comfort F. Abraham <Comfort F. Abraham@DESKTOP-4GJ9PJE>
2024-10-07 14:40:47 +0200

    first commit

---
+++ Overview/MyConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello World!");
+
+        string response;
+        Console.WriteLine("What's your name?");
+        response = Console.ReadLine();
+        Console.WriteLine($"Enjoy the course, {response}");
+
+        OperatingSystem thisOS = Environment.OSVersion;
+        Console.WriteLine(thisOS.Platform);
+        Console.WriteLine(thisOS.VersionString);
+    }
+}
